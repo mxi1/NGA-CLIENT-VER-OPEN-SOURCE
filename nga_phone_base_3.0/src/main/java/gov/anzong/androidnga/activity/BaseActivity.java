2b43356384ef448067a3f4b40e69d744e0d837d2
ximinjun <xi.minjun@gmail.com>
2021-09-30 16:39:25 +0800

    Update default color of the navigation Bar;
    Signed-off-by: ximinjun <xi.minjun@gmail.com>

--- nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/BaseActivity.java
+++ nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/BaseActivity.java
@@ -3,6 +3,7 @@ package gov.anzong.androidnga.activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
@@ -54,6 +55,9 @@ public abstract class BaseActivity extends AppCompatActivity {
         if (mSwipeBackHelper != null) {
             mSwipeBackHelper.onCreate(this);
         }
+
+        // Set Transparent as the default color to match the theme
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
 
         try {
             if (ThemeManager.getInstance().isNightMode()) {
